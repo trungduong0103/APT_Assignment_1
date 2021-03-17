723198799fb9b9a3332fee4a5673ccebaa93bedb
trungduong0103 <trungduong0103@gmail.com>
2021-03-17 17:52:13 +0700

    changed extract_data_from_column signature and added some comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+//#include <math.h>
 
 using namespace std;
 
@@ -10,17 +11,19 @@ enum Column {
 };
 
 unsigned int calculate_array_size(const string &file_name);
-void extract_data_from_column(int array[], const string &file_name, Column column);
-void print_array(int array[], unsigned int array_size);
-long int sum_array(int array[], unsigned int array_size);
 
+void extract_data_from_column(const string &file_name, int array[], Column column);
+
+void print_array(int array[], unsigned int array_size);
+
+long int sum_array(int array[], unsigned int array_size);
 
 int main() {
     const string file_name = "/Users/trung/CLionProjects/APT_Assignment_1/data1.csv";
     const unsigned int array_size = calculate_array_size(file_name);
     int x_column_array[array_size], y_column_array[array_size];
-    extract_data_from_column(x_column_array, file_name, Column::x);
-    extract_data_from_column(y_column_array, file_name, Column::y);
+    extract_data_from_column(file_name, x_column_array, Column::x);
+    extract_data_from_column(file_name, y_column_array, Column::y);
 }
 
 void print_array(int array[], unsigned int array_size) {
@@ -56,11 +59,11 @@ unsigned int calculate_array_size(const string &file_name) {
     return count;
 }
 
-bool string_is_digits(const string& digits) {
+bool string_is_digits(const string &digits) {
     return digits.find_first_not_of("0123456789");
 }
 
-void extract_data_from_column(int array[], const string &file_name, Column column) {
+void extract_data_from_column(const string &file_name, int array[], Column column) {
     int index = 0;
     ifstream infile(file_name);
     if (!infile) {
@@ -72,8 +75,10 @@ void extract_data_from_column(int array[], const string &file_name, Column colum
     while (getline(infile, line)) {
         // find the first occurrence of ',' to extract column numbers
         int index_to_comma = line.find(',');
+        // x_value comes from beginning of array to index of ','
         string x_value = line.substr(0, index_to_comma);
-        string y_value = line.substr(index_to_comma+1, line.length());
+        // y_value comes from beginning of ',' to end of line
+        string y_value = line.substr(index_to_comma + 1, line.length());
         // 2 cases: x column and y column
         switch (column) {
             case x:
@@ -84,7 +89,6 @@ void extract_data_from_column(int array[], const string &file_name, Column colum
                 array[index] = stoi(x_value);
                 break;
             case y:
-                // ignore ','
                 if (!string_is_digits(y_value)) {
                     cerr << "Data is in incorrect format!\n";
                     return;
@@ -93,6 +97,7 @@ void extract_data_from_column(int array[], const string &file_name, Column colum
                 break;
             default:
                 cerr << "column must be of Column enum!" << endl;
+                break;
         }
         index++;
     }
