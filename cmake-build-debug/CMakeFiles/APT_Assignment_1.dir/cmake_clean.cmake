33b6cbaae30641437c8f84505da0058f8bdb9ea0
trungduong0103 <trungduong0103@gmail.com>
2021-03-17 10:50:45 +0700

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/APT_Assignment_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "APT_Assignment_1"
+  "APT_Assignment_1.pdb"
+  "CMakeFiles/APT_Assignment_1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/APT_Assignment_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/APT_Assignment_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for APT_Assignment_1.
+# This may be replaced when dependencies are built.
