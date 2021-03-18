15e5528bbe430526764b62e17159242a2ae49cfd
trungduong0103 <trungduong0103@gmail.com>
2021-03-18 18:45:12 +0700

    add merge sort

--- main.cpp
+++ main.cpp
@@ -10,22 +10,6 @@ enum Column {
     y
 };
 
-unsigned int calculate_array_size(const string &file_name);
-
-void extract_data_from_column(const string &file_name, int array[], Column column);
-
-void print_array(int array[], unsigned int array_size);
-
-long int sum_array(int array[], unsigned int array_size);
-
-int main() {
-    const string file_name = "/Users/trung/CLionProjects/APT_Assignment_1/data1.csv";
-    const unsigned int array_size = calculate_array_size(file_name);
-    int x_column_array[array_size], y_column_array[array_size];
-    extract_data_from_column(file_name, x_column_array, Column::x);
-    extract_data_from_column(file_name, y_column_array, Column::y);
-}
-
 void print_array(int array[], unsigned int array_size) {
     for (int *p = array; p < array + array_size; p++) {
         cout << *p << "\n";
@@ -47,7 +31,7 @@ unsigned int calculate_array_size(const string &file_name) {
         cerr << "Cannot read file: " << file_name << " !\n";
         return 1;
     }
-    int count = 0;
+    unsigned int count = 0;
     string line;
     // ignore the first column (x,y)
     getline(infile, line);
@@ -103,6 +87,132 @@ void extract_data_from_column(const string &file_name, int array[], Column colum
     }
     infile.close();
 }
+
+void merge(int arr[], unsigned int l, unsigned int m, unsigned int r) {
+    unsigned int n1 = m - l + 1;
+    unsigned int n2 = r - m;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+
+    // Initial index of first subarray
+    int i = 0;
+
+    // Initial index of second subarray
+    int j = 0;
+
+    // Initial index of merged subarray
+    unsigned int k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+void merge_sort(int arr[], unsigned int l, unsigned int r) {
+    if (l >= r) {
+        return;//returns recursively
+    }
+    unsigned int m = l + (r - l) / 2;
+    merge_sort(arr, l, m);
+    merge_sort(arr, m + 1, r);
+    merge(arr, l, m, r);
+}
+
+string calculate_mode(int array[], unsigned int array_size) {
+    string modes;
+    print_array(array, array_size);
+    // Initialize modes variable to store all the mode value
+//    vector<int> modes;
+    // Assign temporary value at the first value in the vector
+
+    int tempValue = array[0];
+    // Initialize current_count with 1
+    int currentCount = 1;
+    // Initialize maxCount variable to store the max count
+    int maxCount = 0;
+
+    for (int i = 1; i < array_size; i++) {
+        int currentValue = array[i];
+//        cout << currentValue << endl;
+        if (currentValue == tempValue) {
+            currentCount++;
+        } else {
+            if (currentCount == maxCount) {
+                modes.push_back(tempValue);
+            } else if (currentCount > maxCount) {
+                maxCount = currentCount;
+                modes.clear();
+                modes.push_back(tempValue);
+            }
+            currentCount = 1;
+        }
+        tempValue = currentValue;
+    }
+
+    if (currentCount == maxCount) {
+        modes.push_back(tempValue);
+    } else if (currentCount > maxCount) {
+        maxCount = currentCount;
+        modes.clear();
+        modes.push_back(tempValue);
+    }
+
+    return modes;
+}
+
+int main() {
+    const string file_name = "/Users/trung/CLionProjects/APT_Assignment_1/data1.csv";
+    const unsigned int ARRAY_SIZE = calculate_array_size(file_name);
+    int x_column_array[ARRAY_SIZE], y_column_array[ARRAY_SIZE];
+    extract_data_from_column(file_name, x_column_array, Column::x);
+    calculate_mode(x_column_array, ARRAY_SIZE);
+//    merge_sort(x_column_array, 0, ARRAY_SIZE);
+//    print_array(x_column_array, ARRAY_SIZE);
+//    cout << calculate_mode(x_column_array, ARRAY_SIZE) << endl;
+
+//    merge_sort(x_column_array, 0, ARRAY_SIZE - 1);
+//    print_array(x_column_array, ARRAY_SIZE);
+//    extract_data_from_column(file_name, y_column_array, Column::y);
+//    merge_sort(y_column_array, 0, ARRAY_SIZE - 1);
+//    print_array(y_column_array, ARRAY_SIZE);
+}
+
+
 
 
 
