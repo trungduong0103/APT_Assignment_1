955613d0c436abf9ba7c260c604bc209087affa5
trungduong0103 <trungduong0103@gmail.com>
2021-04-06 15:56:38 +0700

    comment out test functions

--- headers/test_functions.h
+++ headers/test_functions.h
@@ -1,11 +1,11 @@
-#ifndef APT_ASSIGNMENT_1_TEST_FUNCTIONS_H
-#define APT_ASSIGNMENT_1_TEST_FUNCTIONS_H
-
-void display_statistics_calculation_time(const char *FILE_PATH, int (*function)(const char *));
-
-double generate_random_double(double min, double max);
-
-bool generate_sample_data(const std::string &FILE_PATH, unsigned long int num_of_columns, double lower_bound,
-                          double upper_bound);
-
-#endif //APT_ASSIGNMENT_1_TEST_FUNCTIONS_H
+//#ifndef APT_ASSIGNMENT_1_TEST_FUNCTIONS_H
+//#define APT_ASSIGNMENT_1_TEST_FUNCTIONS_H
+//
+//void display_statistics_calculation_time(const char *FILE_PATH, int (*function)(const char *));
+//
+//double generate_random_double(double min, double max);
+//
+//bool generate_sample_data(const std::string &FILE_PATH, unsigned long int num_of_columns, double lower_bound,
+//                          double upper_bound);
+//
+//#endif //APT_ASSIGNMENT_1_TEST_FUNCTIONS_H
--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include "headers/helpers.h"
 #include "headers/validation.h"
-#include "headers/test_functions.h"
+// for testing
+//#include "headers/test_functions.h"
 
 int main(int argc, char *argv[]) {
-//    const std::string FILE_PATH = "/Users/trung/CLionProjects/APT_Assignment_1/data/data4.csv";
-//    generate_sample_data(FILE_PATH, 100000, -50000, 50000);
+    // for creating sample data
+    // const std::string FILE_PATH = "/Users/trung/CLionProjects/APT_Assignment_1/data/data4.csv";
+    // generate_sample_data(FILE_PATH, 100000, -50000, 50000);
 
     if (!cli_input_is_correct(argc, argv)) {
         return 1;
     }
 
-    display_statistics_calculation_time(argv[1], &main_driver_function);
-//    main_driver_function(argv[1]);
+    // for recording execution time
+    // display_statistics_calculation_time(argv[1], &main_driver_function);
+    main_driver_function(argv[1]);
     return 0;
 }
